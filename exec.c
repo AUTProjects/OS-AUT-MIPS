dce5831ee5c8d1dbe062e7bd5170bdd8e9f4740f
pesher <sepehr.sabour@gmail.com>
2016-01-15 13:14:56 +0330

    losed :(

--- exec.c
+++ exec.c
@@ -18,6 +18,8 @@ exec(char *path, char **argv)
   struct proghdr ph;
   pde_t *pgdir, *oldpgdir;
 
+
+
   begin_op();
   if((ip = namei(path)) == 0){
     end_op();
@@ -25,34 +27,42 @@ exec(char *path, char **argv)
   }
   ilock(ip);
   pgdir = 0;
-
   // Check ELF header
-  if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf))
+  if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf)) {
     goto bad;
-  if(elf.magic != ELF_MAGIC)
+  }
+  if(elf.magic != ELF_MAGIC) {
     goto bad;
+  }
 
-  if((pgdir = setupkvm()) == 0)
+  if((pgdir = setupkvm()) == 0) {
     goto bad;
+  }
 
   // Load program into memory.
   sz = 0;
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
-    if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
+    if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph)) {
       goto bad;
-    if(ph.type != ELF_PROG_LOAD)
+    }
+    if(ph.type != ELF_PROG_LOAD) {
       continue;
-    if(ph.memsz < ph.filesz)
+    }
+    if(ph.memsz < ph.filesz) {
       goto bad;
-    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
+    }
+    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0) {
       goto bad;
-    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
+    }
+    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0) {
       goto bad;
+    }
   }
   iunlockput(ip);
   end_op();
   ip = 0;
 
+  cprintf("\n%s\n",path);
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
@@ -97,6 +107,110 @@ exec(char *path, char **argv)
   return 0;
 
  bad:
+  if(pgdir)
+    freevm(pgdir);
+  if(ip){
+    iunlockput(ip);
+    end_op();
+  }
+  return -1;
+}
+
+
+
+int
+exec2(char *path)
+{
+  char *s, *last;
+  int i, off;
+  uint  sz, sp, ustack[3+MAXARG+1];
+  struct elfhdr elf;
+  struct inode *ip;
+  struct proghdr ph;
+  pde_t *pgdir, *oldpgdir;
+
+
+
+  begin_op();
+  if((ip = namei(path)) == 0){
+    end_op();
+    return -1;
+  }
+
+  ilock(ip);
+  pgdir = 0;
+  // Check ELF header
+  if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf)) {
+    goto bad;
+  }
+  if(elf.magic != ELF_MAGIC) {
+    goto bad;
+  }
+
+  if((pgdir = setupkvm()) == 0) {
+    goto bad;
+  }
+
+  // Load program into memory.
+  sz = 0;
+  for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
+    if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph)) {
+      goto bad;
+    }
+    if(ph.type != ELF_PROG_LOAD) {
+      continue;
+    }
+    if(ph.memsz < ph.filesz) {
+      goto bad;
+    }
+    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0) {
+      goto bad;
+    }
+    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0) {
+      goto bad;
+    }
+  }
+  iunlockput(ip);
+  end_op();
+  ip = 0;
+
+  cprintf("\n%s\n",path);
+  // Allocate two pages at the next page boundary.
+  // Make the first inaccessible.  Use the second as the user stack.
+  sz = PGROUNDUP(sz);
+  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+    goto bad;
+  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  sp = sz;
+
+
+  ustack[3+0] = 0;
+
+  ustack[0] = 0xffffffff;  // fake return PC
+  ustack[1] = 0;
+  ustack[2] = sp - (0+1)*4;  // argv pointer
+
+  sp -= (3+0+1) * 4;
+  if(copyout(pgdir, sp, ustack, (3+0+1)*4) < 0)
+    goto bad;
+
+  // Save program name for debugging.
+  for(last=s=path; *s; s++)
+    if(*s == '/')
+      last = s+1;
+  safestrcpy(proc->name, last, sizeof(proc->name));
+
+  // Commit to the user image.
+  oldpgdir = proc->pgdir;
+  proc->pgdir = pgdir;
+  proc->sz = sz;
+  proc->tf->eip = elf.entry;  // main
+  proc->tf->esp = sp;
+  switchuvm(proc);
+  freevm(oldpgdir);
+  return 0;
+
+  bad:
   if(pgdir)
     freevm(pgdir);
   if(ip){
